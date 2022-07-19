b0dfe4058b8c89587299ca133deb37714d53a4d0
tangina3422 <putangina3422@gmail.com>
2022-07-19 16:38:31 +0700

    init

---
+++ spec/tutorial-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TutorialPackageView from '../lib/tutorial-package-view';
+
+describe('TutorialPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
